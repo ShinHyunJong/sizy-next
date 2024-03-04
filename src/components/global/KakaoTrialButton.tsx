fdd734eb8eb711fff27dc41c195829ed67d2cb97
ShinHyunJong <tonyshin@raondata.ai>
2024-03-05 00:29:21 +0900

    fix

--- src/components/global/KakaoTrialButton.tsx
+++ src/components/global/KakaoTrialButton.tsx
@@ -1,10 +1,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { Box, Spinner } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 
 function KakaoTrialButton() {
-  const [redirecting, setRedirecting] = useState(false);
   const router = useRouter();
   const handleTrial = async () => {
     if (typeof window === 'undefined') return;
@@ -24,7 +22,7 @@ function KakaoTrialButton() {
       rounded="lg"
       bg="#F7E600"
     >
-      {redirecting ? <Spinner></Spinner> : '카카오로 3초만에 신청하기'}
+      카카오로 3초만에 신청하기
     </Box>
   );
 }
