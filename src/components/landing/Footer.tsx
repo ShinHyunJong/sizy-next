4d53340f09c5240b8ecd1fce54047fbbb05fa8d8
ShinHyunJong <ian@therovers.net>
2026-02-10 10:00:59 +0900

    주소변경

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
           <Text>(주)풀릿 | 대표 신현종 | 사업자등록번호 295-86-02196</Text>
           <Text>이메일 cs@pulit.co.kr | 전화 070-7807-6505</Text>
           <Text>통신판매업번호 : 2022-서울송파-1003</Text>
-          <Text>서울특별시 송파구 중대로 207, 2F a249호</Text>
+          <Text>서울특별시 송파구 중대로 207, 201동 A249호</Text>
           <Flex mt={4} gap={4}>
             <Link href="terms">
               <Text decoration="underline">이용 약관</Text>
